e7e9ee2ebd723f07c1a267c95dcce68317be21e9
Karina Pliego <karinapliegovergara@gmail.com>
2023-08-14 22:48:26 -0500

    Completed 1/4challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach(image => addingOurImage(image));
+    });
+
+function addingOurImage(dogUrl) {
+    let imgElement = document.createElement('img');
+    imgElement.src = dogUrl;
+
+    let container = document.getElementById('dog-image-container');
+    container.appendChild(imgElement);
+}
+
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+    .then(data => {
+        //data
+    })
